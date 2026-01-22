40033c2db0bbdfc531f018ed7cf36d2237656968
NTPCMD <newtownpunjabi@gmail.com>
2026-01-22 15:10:05 +0800

    Update vite.config.ts

--- portal/vite.config.ts
+++ portal/vite.config.ts
@@ -1,14 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: path.resolve(__dirname),
-  base: "/portal/",
+  base: '/portal/',
   build: {
-    outDir: path.resolve(__dirname, "../dist/portal"),
-    emptyOutDir: true,
-  },
-  publicDir: path.resolve(__dirname, "public"),
-});
+    outDir: '../dist/portal',
+    emptyOutDir: false
+  }
+})
